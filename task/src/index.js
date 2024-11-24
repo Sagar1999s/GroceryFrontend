52d5b069bbbcd98fa9be34fe17f676a31b367031
Sagar Singh <sample2mail@protonmail.com>
2024-11-24 20:24:06 +0530

    commenting webvitals

--- task/src/index.js
+++ task/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import AppRoutes from './routes/AppRoutes';
 
 
@@ -15,4 +15,4 @@ root.render(
 );
 
 
-reportWebVitals();
+// reportWebVitals();
