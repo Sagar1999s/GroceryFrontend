f95226531162e4bcee389839053a2acb7304bd88
Sagar Singh <sample2mail@protonmail.com>
2024-11-24 20:10:04 +0530

    manageing build

--- task/src/index.js
+++ task/src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRoutes from './routes/AppRoutes';
 
-// Get the root element
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the app with the root element
+
 root.render(
   <React.StrictMode>
     <AppRoutes />
   </React.StrictMode>
 );
 
-// Optional: Start measuring performance in your app
+
 reportWebVitals();
